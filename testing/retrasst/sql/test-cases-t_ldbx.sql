4e81425b3c8f7c9a8eb3c3580a0443f14f110a3f
Chris Bird <chris.bird@ctsu.ox.ac.uk>
2014-04-08 16:40:24 +0100

    modifying sql scripts for 2.7.2 database

--- testing/retrasst/sql/test-cases-t_ldbx.sql
+++ testing/retrasst/sql/test-cases-t_ldbx.sql
@@ -18,24 +18,25 @@ if non-defaulted columns are not specified as targets, all column values must be
 E_US0AD7 INSERT: number of target columns must equal the number of specified values.
     box_cid, box_type_cid, box_capacity, external_name, status, slot_cid, time_stamp, note_exists, process_cid [barcode] */
     
---insert into box_name (box_cid, box_type_cid, box_capacity, external_name, status, slot_cid, time_stamp, note_exists, process_cid) values ( -111, 101, 0, 'Example retrieval box 1',2,0,'now',0,0);
---insert into box_name (box_cid, box_type_cid, box_capacity, external_name, status, slot_cid, time_stamp, note_exists, process_cid) values ( -112, 101, 0, 'Example retrieval box 2',2,0,'now',0,0);
---insert into box_name (box_cid, box_type_cid, box_capacity, external_name, status, slot_cid, time_stamp, note_exists, process_cid) values ( -113, 101, 0, 'Example retrieval box 3',2,0,'now',0,0);
---insert into box_name (box_cid, box_type_cid, box_capacity, external_name, status, slot_cid, time_stamp, note_exists, process_cid) values ( -114, 101, 0, 'Example retrieval box 4',2,0,'now',0,0);
---insert into box_name (box_cid, box_type_cid, box_capacity, external_name, status, slot_cid, time_stamp, note_exists, process_cid) values ( -115, 101, 0, 'Example retrieval box 5',2,0,'now',0,0);
---insert into box_name (box_cid, box_type_cid, box_capacity, external_name, status, slot_cid, time_stamp, note_exists, process_cid) values ( -116, 101, 0, 'Example retrieval box 6',2,0,'now',0,0);
---insert into box_name (box_cid, box_type_cid, box_capacity, external_name, status, slot_cid, time_stamp, note_exists, process_cid) values ( -117, 101, 0, 'Example retrieval box 7',2,0,'now',0,0);
---insert into box_name (box_cid, box_type_cid, box_capacity, external_name, status, slot_cid, time_stamp, note_exists, process_cid) values ( -118, 101, 0, 'Example retrieval box 8',2,0,'now',0,0);
---insert into box_name (box_cid, box_type_cid, box_capacity, external_name, status, slot_cid, time_stamp, note_exists, process_cid) values ( -119, 101, 0, 'Example retrieval box 9',2,0,'now',0,0);
---\p\g
+insert into box_name (box_cid, box_type_cid, box_capacity, external_name, status, slot_cid, time_stamp, note_exists, process_cid) values ( -111, -623410, 0, 'Example retrieval box 1',2,0,'now',0,0);
+insert into box_name (box_cid, box_type_cid, box_capacity, external_name, status, slot_cid, time_stamp, note_exists, process_cid) values ( -112, -623410, 0, 'Example retrieval box 2',2,0,'now',0,0);
+insert into box_name (box_cid, box_type_cid, box_capacity, external_name, status, slot_cid, time_stamp, note_exists, process_cid) values ( -113, -623410, 0, 'Example retrieval box 3',2,0,'now',0,0);
+insert into box_name (box_cid, box_type_cid, box_capacity, external_name, status, slot_cid, time_stamp, note_exists, process_cid) values ( -114, -623410, 0, 'Example retrieval box 4',2,0,'now',0,0);
+insert into box_name (box_cid, box_type_cid, box_capacity, external_name, status, slot_cid, time_stamp, note_exists, process_cid) values ( -115, -623410, 0, 'Example retrieval box 5',2,0,'now',0,0);
+insert into box_name (box_cid, box_type_cid, box_capacity, external_name, status, slot_cid, time_stamp, note_exists, process_cid) values ( -116, -623410, 0, 'Example retrieval box 6',2,0,'now',0,0);
+insert into box_name (box_cid, box_type_cid, box_capacity, external_name, status, slot_cid, time_stamp, note_exists, process_cid) values ( -117, -623410, 0, 'Example retrieval box 7',2,0,'now',0,0);
+insert into box_name (box_cid, box_type_cid, box_capacity, external_name, status, slot_cid, time_stamp, note_exists, process_cid) values ( -118, -623410, 0, 'Example retrieval box 8',2,0,'now',0,0);
+insert into box_name (box_cid, box_type_cid, box_capacity, external_name, status, slot_cid, time_stamp, note_exists, process_cid) values ( -119, -623410, 0, 'Example retrieval box 9',2,0,'now',0,0);
+\p\g
 
 --drop sequence temp_box_position;
 create sequence temp_box_position start with 11101;
 \p\g
 
 insert into cryovial_store (record_id, cryovial_id, box_cid, cryovial_position, time_stamp, status, note_exists)
-select -s.cryovial_id, s.cryovial_id, -(next value for temp_box_position)/100,  mod(next value for temp_box_position,100),'now',0,0
-from cryovial_store s, cryovial c where s.cryovial_id = c.cryovial_id and aliquot_type_cid = -31781 and cryovial_barcode like '112155%';
+select -s.cryovial_id, s.cryovial_id, -(next value for temp_box_position)/100, mod(next value for temp_box_position,100),'now',0,0
+from cryovial_store s, cryovial c 
+where s.cryovial_id = c.cryovial_id and aliquot_type_cid = -623410 and cryovial_barcode like '112155%';
 \p\g
 
 -- select the above: select count(*) from cryovial_store s, cryovial c where s.cryovial_id = c.cryovial_id and aliquot_type_cid = -31781 and cryovial_barcode like '112155%' -- 837 
